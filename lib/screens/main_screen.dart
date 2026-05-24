188731bb6192c9f84eb93874a36f4a8df6e98bb8
IATec - Bonnier Nils Mamani Larico <bonnier.larico@iatec.com>
2026-05-24 16:23:54 -0300

    producto provider

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -8,42 +8,52 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Sistema de Ventas")),
-      drawer: Drawer(
-
+      appBar: AppBar(
+        title: Text("Sistema de Ventas"),
+        backgroundColor: Colors.orange,
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CategoryListScreen(),
-                    ),
-                  );
-                },
-                child: Text("Categorias"),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.orange),
+              child: Text(
+                "Sistema de Ventas",
+                style: TextStyle(color: Colors.white, fontSize: 20),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (builder) => ProductListScreen(),
-                    ),
-                  );
-                },
-                child: Text("Productos"),
-              ),
-            ],
-          ),
-          Row(
-            children: [ElevatedButton(onPressed: () {}, child: Text("Ventas"))],
-          ),
-        ],
+            ),
+            ListTile(
+              leading: Icon(Icons.category),
+              title: Text("Categorías"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => CategoryListScreen(),
+                ));
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.shopping_bag),
+              title: Text("Productos"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => ProductListScreen(),
+                ));
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.point_of_sale),
+              title: Text("Ventas"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text("Bienvenido al Sistema de Ventas"),
       ),
     );
   }
